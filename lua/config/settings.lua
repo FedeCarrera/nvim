0b2d9a2cdd1783aa6acffa217e771580c5c7b201
efc <carrera.federico@gmail.com>
2024-05-04 18:49:29 -0300

    agregado el plugin git signs

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,4 +1,5 @@
 vim.o.number = true
 vim.o.relativenumber = true
 -- vim.cmd.colorscheme("slate")
+vim.g.mapleader = " " -- Esto setea la barra espaciadora como tecla Leader
 
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,8 @@
 return { 
 	"tpope/vim-fugitive",
-
+	cmd ={"G", "Git"},
+	keys = {
+		{"<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch"},
+		{"<leader>gc", ":Git pull<cr>", desc = "Git pull"},
+	},		
 }
